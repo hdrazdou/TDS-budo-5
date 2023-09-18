941d438ba9429d07a7c5f9925a51de644b24d80f
Hleb Drazdou <Hleb.Drazdou@gmail.com>
2023-09-18 23:44:33 +0200

    added patroling script

---
+++ Assets/Scripts/Game/Enemy/Base/EnemyPatroling.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+namespace TDS.Game.Enemy
+{
+    public class EnemyPatroling : EnemyDirectMovement
+    {
+        #region Variables
+
+        [Header("Settings")]
+        [SerializeField] private bool _isPatroling;
+        [SerializeField] private Vector3 _firstPoint;
+        [SerializeField] private Vector3 _secondPoint;
+        
+        [Header("Components")]
+        [SerializeField] private EnemyMovement _enemyMovement;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            _firstPoint = transform.position;
+
+            if (_isPatroling)
+            {
+                _enemyMovement.GoToPoint(_secondPoint);
+            }
+        }
+
+        // private void Update()
+        // {
+        //     if (!_enemyMovement.isMovingToPoint)
+        //     {
+        //         _enemyMovement.GoToPoint(_firstPoint);
+        //     }
+        // }
+
+        #endregion
+
+        #region Public methods
+
+        public override void GoToPoint(Vector3 point)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
